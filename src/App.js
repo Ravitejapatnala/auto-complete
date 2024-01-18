d55f1eb5ad8e496f905bc866fa8469e0b8ea83f4
akhil <akhil.ce08@gmail.com>
2024-01-18 15:56:13 +0530

    first commit and uploaded files

--- src/App.js
+++ src/App.js
@@ -1,25 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import fruits from "./Components/data";
 
 function App() {
+  const[fruit, setFruit]= useState("")
+  const[suggesstions, setSuggestions]= useState([])
+
+  // Here fruit is initially an empty string, and item.startsWith(fruit) will return true for all items in the
+  // fruits array because every string starts with an empty string. Hence, all fruits are included in the 
+  // suggestions array and displayed on the page.Once a user starts typing, fruit will no longer be an empty 
+  // string, and item.startsWith(fruit) will only return true for fruits that start with the user’s input, 
+  // updating suggestions accordingly.
+
+  useEffect(()=>{
+    setSuggestions(fruits.filter((item)=>(
+      item.startsWith(fruit)
+    )))
+  }, [fruit])
+
+  function handleInputChange(e){
+    setFruit(e.target.value);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input type="text" placeholder="Search fruit..." onChange={handleInputChange} ></input>
+      <ul>
+        {
+          suggesstions.map((item)=>(
+            <li>{item}</li>
+          ))
+        }
+      </ul>
     </div>
-  );
+  )
 }
 
 export default App;
---
+++ src/Components/data.js
@@ -0,0 +1,3 @@
+const fruits = ["apple", "banana", "cherry", "date", "elderberry", "fig"]
+
+export default fruits;
